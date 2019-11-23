4e96bb314eec2729dccf489ca5bef0b9515d751b
rachfuerst <rach.fuerst@gmail.com>
2019-11-23 12:03:05 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,17 @@ class StudentsController < ApplicationController
   end
 
   def create
+    # Create a new student object
+    @student= Student.new
+    # Set the first_name attribute of @student equal to what was entered as #first_name in the form
+    @student.first_name= params[:first_name]
+    # See above
+    @student.last_name= params[:last_name]
+    # Save these attributes to the database
+    @student.save
+
+    #redirect the user to a page that displays their content (the #show path/action)
+    redirect_to student_path(@student)
   end
 
 end
